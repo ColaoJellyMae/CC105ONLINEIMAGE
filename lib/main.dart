ffe2768985762b68183536ab940eea3ed5917f1a
ColaoJellyMae <bg201802353@wmsu.edu.ph>
2020-11-04 13:08:28 -1200

    OfflineImage_AppIcon

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,7 @@ void main() {
           backgroundColor: Colors.black45,
         ),
         body: Image(
-          image: NetworkImage(
-              'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.kindpng.com%2Fpicc%2Fm%2F79-794878_diamond-jokingart-com-transparent-background-diamond-clipart-hd.png&imgrefurl=https%3A%2F%2Fwww.kindpng.com%2Fimgv%2FobxwbT_diamond-jokingart-com-transparent-background-diamond-clipart-hd%2F&tbnid=Hag1YFOIJZNf0M&vet=12ahUKEwj0lMipmrHsAhUM5ZQKHRUaB3UQMygGegUIARDIAQ..i&docid=MdN22a6U-H0Q4M&w=860&h=956&q=diamond%20clipart%20png&ved=2ahUKEwj0lMipmrHsAhUM5ZQKHRUaB3UQMygGegUIARDIAQ'),
+          image: AssetImage('images/Screenshot_2.png'),
         ),
       ),
     ),
